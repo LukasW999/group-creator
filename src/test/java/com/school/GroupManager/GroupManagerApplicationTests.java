5080ba4da6f0dd6e7893baf3068960ff4c0023c8
lukas <lukaswenzel0909@gmail.com>
2023-11-20 16:40:53 +0100

    First init (not working)

---
+++ src/test/java/com/school/GroupManager/GroupManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.GroupManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
